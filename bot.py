5437c3d36565a989be1856caf99769fa9b11d10a
soimmary <55881643+soimmary@users.noreply.github.com>
2021-06-18 15:26:45 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,11 +15,16 @@ def send_welcome(message):
 
 @bot.message_handler(commands=["geo"])
 def geo(message):
-    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-    button_geo = types.KeyboardButton(text="Отправить местоположение", request_location=True)
-    keyboard.add(button_geo)
+    keyboard = types.ReplyKeyboardMarkup(True, True)
+    keyboard.row('Отправить местоположение', request_location=True)
     bot.send_message(message.chat.id, "Привет! Нажми на кнопку и передай мне свое местоположение", reply_markup=keyboard)
+    bot.register_next_step_handler(message, )
 
+
+def print_geo(message):
+    bot.send_message(message.chat.id, message.location)
+    bot.send_message(message.chat.id, "latitude: %s; longitude: %s" % (message.location.latitude, message.location.longitude))
+    
 
 @bot.message_handler(content_types=["location"])
 def location(message):
