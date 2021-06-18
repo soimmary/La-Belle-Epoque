b57dd35151ca3b95ccab6c99c062e1a6145341e0
soimmary <55881643+soimmary@users.noreply.github.com>
2021-06-18 15:59:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 import telebot
 from telebot import types
+import token
 
-token = "1678532360:AAG3iNv8KVbAfv61Ss6RQi-O1YdzCdI_AEg"
 
-bot = telebot.TeleBot(token)
+bot = telebot.TeleBot(token.TOKEN)
 
 
 @bot.message_handler(commands=['start', 'help'])
