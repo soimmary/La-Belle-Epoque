e254def746769067550b2ab5972bc58cbd38b2fa
soimmary <55881643+soimmary@users.noreply.github.com>
2021-06-18 15:11:34 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,13 @@ token = "1678532360:AAG3iNv8KVbAfv61Ss6RQi-O1YdzCdI_AEg"
 bot = telebot.TeleBot(token)
 
 
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.send_message(message.chat.id,
+                     "Привет! Этот бот показывает ближайшие места на карте, связанные с "
+                     "прекрасным Серебряным веком!")
+
+
 @bot.message_handler(commands=["geo"])
 def geo(message):
     keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
@@ -13,7 +20,7 @@ def geo(message):
     keyboard.add(button_geo)
     bot.send_message(message.chat.id, "Привет! Нажми на кнопку и передай мне свое местоположение", reply_markup=keyboard)
 
-    
+
 @bot.message_handler(content_types=["location"])
 def location(message):
     if message.location is not None:
@@ -21,4 +28,5 @@ def location(message):
         print("latitude: %s; longitude: %s" % (message.location.latitude, message.location.longitude))
 
 
-bot.polling()
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
